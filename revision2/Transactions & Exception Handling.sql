c3d4e640f3dde1a43bae013e17de21123262bb97
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-07-08 12:35:31 +0530

    revision 2 transactions

---
+++ revision2/Transactions & Exception Handling.sql
@@ -0,0 +1,29 @@
+--Phase 16 – Transactions & Exception Handling
+
+--Very important for office review.
+
+--Q86
+
+--Money transfer with COMMIT.
+
+--Q87
+
+--Money transfer with ROLLBACK.
+
+--Q88
+
+--TRY-CATCH logging.
+
+--Q89
+
+--Custom THROW error.
+
+--Q90
+
+--Nested TRY-CATCH.
+
+--Q91
+
+--Stock/order processing transaction.
+
+--These directly match your exception-handling tasks.
